4a29a4fdf7be13c0777c4c372da906a15a318e48
Sagar <dev.sagartmg@gmail.com>
2024-03-26 15:26:32 +0545

    functionas

--- js/index.js
+++ js/index.js
@@ -1,107 +1,42 @@
-/*  variables */
-/*  data-types*/
+/* array,object */
+/* functions
+        - reusable.
+ */
 
-/* array
-        - index
-*/
-/* object 
-        - key
+/* comments
+    ctrl + /
+    alt + shift + a    
+
 */
 
-let product = {
-    name: "projector",
-    rate: 500000,
+/**
+ * function which print dobule of input
+ * @param {number} input
+ */
+function double(input) {
+    console.log(`${input} * 2 = ${input * 2}`)
 }
 
-product.price = product.rate
-delete product.rate
+double(100)
+double(200)
+double(400)
 
-console.log(product)
 
-let productApiData = {
-    products: [
-        { name: "watch", price: 1000 },
-        { name: "mouse", price: 1000 },
-        { name: "cover", price: 100 },
-        { name: "mobile", rate: 100000 }, // convert rate field to price.
-    ],
-}
-
-productApiData.products[3].price = productApiData.products[3].rate
-delete productApiData.products[3].rate
-
-/* wap to calculate sum of two numbers */
-/* 
-    1 + 2 = 3
-    100 + 200 = 300
-    50 + 100 = 150
-*/
-
-/* template literal / string literal */
-/* commment
-        ctrl + /
-        alt + shift + a    // multi line comments
-*/
-
-/* 
-    let firstInput = 1
-    let secondInput = 2
-    let sum = firstInput + secondInput
-    // console.log(firstInput + " + " + secondInput + " = ", sum)
-    console.log(`${firstInput} + ${secondInput} = ${sum}`)
-
-    let input3 = 100
-    let input4 = 300
-    let result = input3 + input4
-    console.log(input3 + " + " + input4 + " = ", result)
-
-    let input5 = 50
-    let input6 = 100
-    let result2 = input5 + input6
-    console.log(input5 + " + " + input6 + " = ", result2) 
-
-*/
-
-/* 
-function
-    just like variable, meant to be re -used
-
-    function <functionName>( parameter1, parameter1 ){
-        // do something
+let colors = [
+    {
+        name:"red",
+        hexValue:"#FFEE00",
     }
-
-    calling/run/execute  a function
-    
-    <functionName>()
-    <functionName>()
-    <functionName>()
-
-*/
-
-function calculateSum(input1, input2) {
-    console.log({ input1: input1 })
-    console.log({ input2 })
-
-    let sum = input1 + input2
-    console.log(`${input1} + ${input2} = ${sum}`)
-}
-
-calculateSum(1, 2) // 1,2
-calculateSum(100, 200) // 100,200
-calculateSum(50, 100) // 50,100
+]
 
 
 let todos = [
     {
         title: "html",
-        status: "complted",
+        status: "pending",
     },
     {
         title: "css",
-        status: "complted",
-    },
-    {
-        title: "js",
         status: "complted",
     },
     {
@@ -114,11 +49,92 @@ let todos = [
     },
 ]
 
+/* 
+    console.log(`${todos[0].title} is ${todos[0].status}`)
+    console.log(`${todos[1].title} is ${todos[1].status}`)
+    console.log(`${todos[2].title} is ${todos[2].status}`)
+    console.log(`${todos[3].title} is ${todos[3].status}`)
+ */
+
+function printStatus(index) {
+    console.log(`${todos[index].title} is ${todos[index].status}`)
+}
+
+printStatus(0)
+printStatus(1)
+printStatus(2)
+printStatus(3)
+
+function printTodoInfo(todo) {
+    console.log(`${todo.title} is ${todo.status}`)
+}
+
+printTodoInfo(todos[0])
+printTodoInfo({ name: "css", status: "complted" })
+printTodoInfo(todos[2])
+printTodoInfo(todos[3])
+
+function greet(name) {
+    console.log(`hello ${name}`)
+}
+
+greet("ram")
+greet("sita")
+greet("hari")
+
+/* sum(100,200) */
+console.log("ram", "sita", "hari")
+
+function activate() {
+    console.log("acivate")
+}
+
+let user = {
+    name: "ram",
+    login: function login() {
+        console.log("logged in ")
+    },
+    logout: function () {
+        console.log("logged out ")
+    },
+    "activate": activate,
+}
+
+console.log(user.name)
+user.login()
+user.logout()
+user.signOut  = user.logout
+user.signOut()
+user.activate()
 
 /* 
-    html is complted
-    css is complted
-    js is complted
-    react is pending
-    express is pending 
-*/
+    let console = {
+        log: function(){
+            
+        }
+    }
+ */
+
+
+    /* 
+        research
+        - arrow function
+        - return
+
+        loop
+            - for loop
+        
+            
+        array functions
+        .forEach
+        .map
+        .filter
+
+    */
+
+
+function sum(inpu1,inpu2){
+
+}
+
+const calculateSum = (inpu1,inpu2) =>{  }
