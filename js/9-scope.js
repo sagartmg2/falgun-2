15c2fe2f31aa7f3618971010e2d50ef9ea107394
Sagar <dev.sagartmg@gmail.com>
2024-03-29 15:28:33 +0545

    opertors

---
+++ js/9-scope.js
@@ -0,0 +1,159 @@
+/* function */
+/* arrow function */
+/* return in  function */
+/* if else */
+
+// const login = () => {
+//     console.log(this);
+//     console.log("logged in")
+// }
+
+function login() {
+    // console.log(this);
+    console.log(this.name, "logged in")
+}
+
+let users = [
+    { name: "ram", login: login },
+    { name: "hari", login: login },
+]
+
+users[0].login()
+users[1].login()
+
+let topics = [
+    {
+        title: "html",
+        status: true,
+    },
+    {
+        title: "css",
+        status: true,
+    },
+    {
+        title: "react",
+        status: false,
+    },
+]
+
+/* falsy values */
+let ourCondition = false
+ourCondition = 0
+ourCondition = "" // full empty string // false
+ourCondition = " " // there is a space here // true value
+ourCondition = null
+ourCondition = undefined
+ourCondition = NaN
+
+/* 
+    except these above 6, all are considered positive value in javascript
+    ourCondition = []
+    ourCondition = {}
+*/
+
+if (ourCondition) {
+    console.log("+ve value")
+} else {
+    console.log("neagative value")
+}
+
+/* falsy values in javascript
+     - false
+     - 0
+
+*/
+
+/* 
+    if (topics[0].status) {
+        console.log(`${topics[0].title} is completed`)
+    } else {
+        console.log(`${topics[0].title} is pending`)
+    }
+
+    if (topics[2].status) {
+        console.log(`${topics[2].title} is completed`)
+    } else {
+        console.log(`${topics[2].title} is pending`)
+    }
+ */
+
+const checkStatus = (topic) => {
+    /* object destructuring */
+    let { status, title } = topic
+
+    // if (topic.status) {
+    //     console.log(`${topic.title} is completed`)
+    // } else {
+    //     console.log(`${topic.title} is pending`)
+    // }
+
+    if (status) {
+        console.log(`${title} is completed`)
+    } else {
+        console.log(`${title} is pending`)
+    }
+}
+
+checkStatus({
+    title: "html",
+    status: true,
+})
+checkStatus(topics[0])
+checkStatus(topics[1])
+checkStatus(topics[2])
+
+/* 
+        html is complted
+        css is complted
+        react is pending 
+    */
+
+/* 
+    research optional
+    - global execution context
+    - call stack
+    - hoisting
+
+    
+    // required
+    - falsy values in js
+ */
+
+/* global scope */
+let brand = "apple"
+
+console.log(double(2))
+function double(input) {
+    let result = input * 2
+    return result
+}
+
+const triple = (input) => {
+    let result = input * 3
+    return result
+}
+console.log(triple(111))
+
+if (true) {
+    let data = "api data"
+    console.log({ data })
+}
+
+if (true) {
+    const data = "api data 2"
+    console.log({ data })
+}
+
+if (true) {
+    /* this data variable is a block scope varirable   */
+    const data = "api data 3"
+}
+
+/* we cannot access block scope varialbe outside the scope */
+console.log({ data })
+
+/* operators
+    logical operators
+     
+*/
+
--- js/index.js
+++ js/index.js
@@ -1,158 +1,127 @@
-/* function */
-/* arrow function */
-/* return in  function */
-/* if else */
-
-// const login = () => {
-//     console.log(this);
-//     console.log("logged in")
-// }
-
-function login() {
-    // console.log(this);
-    console.log(this.name, "logged in")
-}
-
-let users = [
-    { name: "ram", login: login },
-    { name: "hari", login: login },
-]
-
-users[0].login()
-users[1].login()
-
-let topics = [
-    {
-        title: "html",
-        status: true,
-    },
-    {
-        title: "css",
-        status: true,
-    },
-    {
-        title: "react",
-        status: false,
-    },
-]
-
-/* falsy values */
-let ourCondition = false
-ourCondition = 0
-ourCondition = ""
-ourCondition = null
-ourCondition = undefined
-ourCondition = NaN
-
-/* 
-    except these above 6, all are considered positive value in javascript
-    ourCondition = []
-    ourCondition = {}
-*/
-
-if (ourCondition) {
-    console.log("+ve value")
-} else {
-    console.log("neagative value")
-}
-
-/* falsy values in javascript
-     - false
-     - 0
-
-*/
-
-/* 
-    if (topics[0].status) {
-        console.log(`${topics[0].title} is completed`)
-    } else {
-        console.log(`${topics[0].title} is pending`)
-    }
-
-    if (topics[2].status) {
-        console.log(`${topics[2].title} is completed`)
-    } else {
-        console.log(`${topics[2].title} is pending`)
-    }
+/*
+    function 
+    arrow function
+    return in function 
+    if else 
+        nested  if else. 
+    scope
+    object destructuring
  */
 
-const checkStatus = (topic) => {
-    /* object destructuring */
-    let { status, title } = topic
-
-    // if (topic.status) {
-    //     console.log(`${topic.title} is completed`)
-    // } else {
-    //     console.log(`${topic.title} is pending`)
-    // }
-
-    if (status) {
-        console.log(`${title} is completed`)
-    } else {
-        console.log(`${title} is pending`)
-    }
-}
-
-checkStatus({
-    title: "html",
-    status: true,
-})
-checkStatus(topics[0])
-checkStatus(topics[1])
-checkStatus(topics[2])
-
 /* 
-        html is complted
-        css is complted
-        react is pending 
-    */
+operators
+    arthematic 
+        + -
+        * - multiply
+        / - division
+        % - modolus // gives remainder
 
-/* 
-    research optional
-    - global execution context
-    - call stack
-    - hoisting
+        
+    post increment 
+        it updates the value, but we cannot  access the updated value immedeately
+    pre increment 
+        it updates the value, but we can also access the updated value immedeately
+
+    assignment 
+        = 
+
+    comparision 
+         < 
+         >
+         <=
+         >=
+         ==  // equality operator
+         ===   // strict equality operator
 
     
-    // required
-    - falsy values in js
- */
-
-/* global scope */
-let brand = "apple"
-
-console.log(double(2))
-function double(input) {
-    let result = input * 2
-    return result
-}
-
-const triple = (input) => {
-    let result = input * 3
-    return result
-}
-console.log(triple(111))
-
-if (true) {
-    let data = "api data"
-    console.log({ data })
-}
-
-if (true) {
-    const data = "api data 2"
-    console.log({ data })
-}
-
-if (true) {
-    /* this data variable is a block scope varirable   */
-    const data = "api data 3"
-}
-
-/* we cannot access block scope varialbe outside the scope */
-console.log({ data })
-
-/* operators
     logical operators
-     
+        AND   &&
+        OR   ||
+        NOT   !
+
 */
+
+console.log(10 / 3)
+console.log(10 % 2)
+
+let count = 100
+// count = 101
+// count = count  + 1
+// count += 1
+// count++
+// ++count
+
+// console.log(count++,count);
+
+const double = (input) => {
+    return input * 2
+}
+
+// let output = double(count++)
+let output = double(++count)
+console.log({ output })
+console.log({ count })
+
+console.log(100 < 100) // false
+console.log(100 <= 100) // true
+console.log(100 == 100) // true
+console.log("ram" == "Ram")
+console.log("ram" == "ram ")
+console.log("1" == 1) // typecast : data type conversion
+
+console.log("hello" + "world");
+console.log("1" + 1); // numberical 1 is converted to string
+console.log("1" + "1");  // 11
+console.log("1" - 1);  // - is only i numerical operators // "1" is converted to number
+console.log("one" - 1); // NaN
+
+
+console.log("1" === 1) // false // no typecast // check for data types too. 
+
+
+
+
+/* reserach
+    ternary operators
+    reference data types 
+        - object 
+        - arry 
+*/
+
+// console.log([] == []) // false
+// console.log({} == {}) // false
+
+
+let product = {
+    price:"100.50",
+    vat: 15
+}
+
+// let totalPrice = product.price + product.vat
+// let totalPrice = parseFloat(product.price) + product.vat
+// console.log(totalPrice);
+
+
+
+let student = {
+    name:"Ram",
+    paid:false,
+    hasScholoarship:false,
+    hasMetAttendance:true
+}
+
+if(student.paid){
+    console.log("he can  give exam");
+}else {
+    if(student.hasScholoarship){
+        console.log("he can  give exam");
+    }else{
+        console.log("he can not  give exam");
+    }
+}
+
+
+
+/* ram cant give exam   // if none of the condition is true   */
+/* ram can give exam    // if any one of the condition is true  */
 
