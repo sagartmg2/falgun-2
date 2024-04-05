ff8fec3e7b12039a2cc3e0b3610ad3c51e80d0dd
Sagar <dev.sagartmg@gmail.com>
2024-04-05 15:31:17 +0545

    .filter , .map .forEach

---
+++ js/14- forEach.js
@@ -0,0 +1,144 @@
+/* arrow  functions */
+/* array realted functions
+     push
+     pop
+     unshift
+     shift
+     splice
+
+
+     doesnot change original array.
+     slice
+     array.forEach
+     array.filter
+     array.map
+
+*/
+
+let numbers = [4, 5, 3, 2]
+numbers[4] = 1 //  [4, 5, 3, 2,1]
+numbers.push(0)
+console.log(numbers)
+numbers.pop()
+console.log(numbers)
+
+let brands = ["apple", "lg", "sony", "panasonic"]
+brands.splice(1, 2) // ["apple", "panasonic"]
+brands.splice(1, 0, "samsung")
+
+console.log(brands)
+
+let colors = ["Red", "green", "orange", "black"]
+let newColors = colors.slice(1, 4)
+console.log({ colors })
+console.log({ newColors })
+
+/* callback function
+        - 
+*/
+
+const showPopup = () => {
+    console.log("show popup")
+}
+
+setTimeout(showPopup, 2000)
+setTimeout(() => {
+    console.log("show another popup")
+}, 3000)
+
+let names = ["ram", "abc", "xyz", "ijk"]
+
+/* 
+for (let index = 0; index < names.length; index++) {
+    const element = names[index]
+    console.log(element)
+}
+ */
+
+names.forEach((element, index) => {
+    console.log("index -", index, element)
+})
+
+let todos = [
+    {
+        title: "html",
+        status: true,
+    },
+    {
+        title: "css",
+        status: true,
+    },
+    {
+        title: "react",
+        status: false,
+    },
+    {
+        title: "express",
+        status: false,
+    },
+]
+let completedTodos  = []
+
+completedTodos.push({
+    title: "html",
+    status: true,
+})
+
+console.log({completedTodos});
+
+
+
+/* 
+todos.forEach((element) =>{
+    let {title,status}  = element
+    if(status){
+        console.log(`${title} is compoleted`);
+    }else{
+        console.log(`${title} is pending`);
+    }
+}) */
+
+todos.forEach((element) => {
+    let { title, status } = element
+    console.log(`${title} is ${status ? "complted" : "pendng"}`)
+})
+
+
+
+const nepaliNames = ["Aarav", "Aarya", "Aasha", "Anisha"]
+const fakeUsers = []
+
+fakeUsers.push({
+    name:"arrav",
+    email:"araav"+"@gamial.com",
+    password:"arrav"+0
+})
+
+fakeUsers.push({
+    name:"aarya",
+    email:"aarya"+"@gamial.com",
+    password:"aarya"+1
+})
+
+fakeUsers.push({
+    name:"aasha",
+    email:"aasha"+"@gamial.com",
+    password:"aasha"+1
+})
+
+
+/* 
+
+TODO: 
+    From the above array create an new array of users with email and password 
+
+
+    OUTPUT should be like following
+    [
+        { name: 'Aarav', email: 'aarav@gmail.com', password: 'aarav0' },
+        { name: 'Aarya', email: 'aarya@gmail.com', password: 'aarya1' },
+        { name: 'Aasha', email: 'aasha@gmail.com', password: 'aasha2' },
+        { name: 'Anisha', email: 'anisha@gmail.com', password: 'anisha3' },
+    ] 
+
+*/
--- js/index.js
+++ js/index.js
@@ -1,64 +1,3 @@
-/* arrow  functions */
-/* array realted functions
-     push
-     pop
-     unshift
-     shift
-     splice
-
-
-     doesnot change original array.
-     slice
-     array.forEach
-     array.filter
-     array.map
-
-*/
-
-let numbers = [4, 5, 3, 2]
-numbers[4] = 1 //  [4, 5, 3, 2,1]
-numbers.push(0)
-console.log(numbers)
-numbers.pop()
-console.log(numbers)
-
-let brands = ["apple", "lg", "sony", "panasonic"]
-brands.splice(1, 2) // ["apple", "panasonic"]
-brands.splice(1, 0, "samsung")
-
-console.log(brands)
-
-let colors = ["Red", "green", "orange", "black"]
-let newColors = colors.slice(1, 4)
-console.log({ colors })
-console.log({ newColors })
-
-/* callback function
-        - 
-*/
-
-const showPopup = () => {
-    console.log("show popup")
-}
-
-setTimeout(showPopup, 2000)
-setTimeout(() => {
-    console.log("show another popup")
-}, 3000)
-
-let names = ["ram", "abc", "xyz", "ijk"]
-
-/* 
-for (let index = 0; index < names.length; index++) {
-    const element = names[index]
-    console.log(element)
-}
- */
-
-names.forEach((element, index) => {
-    console.log("index -", index, element)
-})
-
 let todos = [
     {
         title: "html",
@@ -77,37 +16,68 @@ let todos = [
         status: false,
     },
 ]
-let completedTodos  = []
 
-completedTodos.push({
-    title: "html",
-    status: true,
-})
-
-console.log({completedTodos});
-
-
-
-/* 
-todos.forEach((element) =>{
-    let {title,status}  = element
-    if(status){
-        console.log(`${title} is compoleted`);
-    }else{
-        console.log(`${title} is pending`);
-    }
-}) */
+let completedTodos = []
+let incompletedTodos = []
 
 todos.forEach((element) => {
-    let { title, status } = element
-    console.log(`${title} is ${status ? "complted" : "pendng"}`)
+    if (element.status) {
+        completedTodos.push(element)
+    } else {
+        incompletedTodos.push(element)
+    }
 })
 
+let finishedTodos = todos.filter((element) => {
+    if (element.status) {
+        return true
+    }
+})
 
+finishedTodos = todos.filter((element) => (element.status ? true : false))
+finishedTodos = todos.filter((element) => element.status)
+
+console.log({ completedTodos })
+console.log({ finishedTodos })
+console.log({ incompletedTodos })
 
 const nepaliNames = ["Aarav", "Aarya", "Aasha", "Anisha"]
 const fakeUsers = []
 
+nepaliNames.forEach((name, index) => {
+    fakeUsers.push({
+        name: name,
+        email: name.toLowerCase() + "@gmail.com",
+        password: name + index,
+    })
+})
+console.log({ fakeUsers })
+
+/* 
+    string function
+        "HELL".toLowerCase()
+        "   HELL   ".trim()
+        "Bearer token".replace("Bearer ","")
+*/
+
+/* 
+fakeUsers.push({
+    name:"arrav",
+    email:"araav"+"@gamial.com",
+    password:"arrav"+0
+})
+
+fakeUsers.push({
+    name:"aarya",
+    email:"aarya"+"@gamial.com",
+    password:"aarya"+1
+})
+
+fakeUsers.push({
+    name:"aasha",
+    email:"aasha"+"@gamial.com",
+    password:"aasha"+1
+}) */
 
 /* 
 
@@ -124,3 +94,49 @@ TODO:
     ] 
 
 */
+
+let numbers = [10, 5, 4, 3, 1, 6, 7]
+let oddNumbers = []
+numbers.forEach((number) => {
+    /* 
+        if (number % 2 == 0) {
+        } else {
+            oddNumbers.push(number)
+        } 
+    */
+
+    if (number % 2 !== 0) {
+        oddNumbers.push(number)
+    }
+})
+
+let evenNumbers = numbers.filter((element) => {
+    if (element % 2 === 0) {
+        return true
+    }
+})
+evenNumbers = numbers.filter((element) => (element % 2 === 0 ? true : false))
+evenNumbers = numbers.filter((element) => element % 2 === 0)
+
+console.log({ evenNumbers })
+
+let dobule = (input) => {
+    return input * 2
+}
+
+dobule = (input) => input * 2
+
+numbers = [10, 5, 4]
+
+let doubledNumbers = []
+numbers.forEach((el) => {
+    doubledNumbers.push(el * 2)
+})
+
+doubledNumbers = numbers.map((el) => {
+    return el * 2
+})
+
+doubledNumbers = numbers.map((el) => el * 2)
+
+console.log(doubledNumbers)
